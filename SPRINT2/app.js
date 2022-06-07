c573783dfb4b82a13406a7db70069a168c4daf8d
JGiudi <jgiudi20@gmail.com>
2022-06-07 17:49:10 -0300

    agregando archvos

---
+++ SPRINT2/app.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+const app= express();
+const puerto = 5100;
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get("/", (req, res)=> {
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+app.get("/home", (req, res)=> {
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.listen(puerto, ()=>{
+    console.log("Esta corriendo en el puerto " + puerto);
+});
+
